12fc7ff0838b8388eda151c2c29a0c0e49f0e603
CristianN01 <cristiannechifor71@gmail.com>
2024-07-30 01:06:17 +0200

    rappresentato i progetti su index tramite una tabella

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,6 +4,44 @@
 <div class="container">
     <div class="row justify-content-center">
         @dump($projects)
+        <div class="col-12">
+            <table class="table table-hover table-success table-striped ">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            #
+                        </th>
+                        <th scope="col">
+                            Title
+                        </th>
+                        <th scope="col">
+                            Author
+                        </th>
+                        <th scope="col">
+                            Date
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <td>
+                                {{ $project->id }}
+                            </td>
+                            <td>
+                                <strong>{{ $project->title }}</strong>
+                            </td>
+                            <td>
+                                {{ $project->author }}
+                            </td>
+                            <td>
+                                {{ $project->date }}
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @endsection
